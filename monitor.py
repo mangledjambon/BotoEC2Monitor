3edf2bdab3de8bbb754be7f58ef80123976ab214
sean <seanbreen91@gmail.com>
2015-07-30 14:28:48 +0100

    added TODO list

--- monitor.py
+++ monitor.py
@@ -1,10 +1,12 @@
 __author__ = 'sean'
 
+# TODO connect to my instance
+# TODO print out all instances and details (AMI ID, instance type, region, time of launch)
+
 import boto.ec2
 
 ec2 = boto.ec2.connect_to_region('us-west-2')
-
-print(ec2.aws_access_key_id)
+print(ec2)
 
 
 
