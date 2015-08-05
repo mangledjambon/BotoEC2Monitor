ecf38369c54dde3e529038acdb478a811db3bc2a
sean <seanbreen91@gmail.com>
2015-08-05 15:10:31 +0100

    connected to region us-west-2

--- monitor.py
+++ monitor.py
@@ -3,10 +3,33 @@ __author__ = 'sean'
 # TODO connect to my instance
 # TODO print out all instances and details (AMI ID, instance type, region, time of launch)
 
+import boto
 import boto.ec2
 
 ec2 = boto.ec2.connect_to_region('us-west-2')
 print(ec2)
+
+key_name = 'EC2_pyCloud'
+key_dir = '~/Downloads'
+
+try:
+    key = ec2.get_all_key_pairs(keynames=[key_name])[0]
+except ec2.ResponseError,e :
+    if e.code == 'InvalidKeyPair.NotFound':
+        print 'Creating KeyPair: %s' % key_name
+
+        key = ec2.create_key_pair(key_name)
+        key.save(key_dir)
+    else:
+        raise
+
+reservations = ec2.run_instances(
+    image_id='ami-d0d8b8e0',
+    key_name=key_name,
+    instance_type='t1.micro',
+    security_groups='ssh-access'
+)
+
 
 
 
