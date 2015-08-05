74f831949e8c5a1ce49070a8ee9c01d6310c7b9d
sean <seanbreen91@gmail.com>
2015-08-05 15:26:33 +0100

    printing some details of the instance

--- monitor.py
+++ monitor.py
@@ -1,35 +1,22 @@
 __author__ = 'sean'
 
-# TODO connect to my instance
 # TODO print out all instances and details (AMI ID, instance type, region, time of launch)
 
-import boto
 import boto.ec2
 
-ec2 = boto.ec2.connect_to_region('us-west-2')
-print(ec2)
+ec2 = boto.ec2.connect_to_region('us-west-2')           # connect to region where instance is located
 
-key_name = 'EC2_pyCloud'
-key_dir = '~/Downloads'
+reservations = ec2.get_all_reservations()               # get list of all reservations
+assert isinstance(reservations[0].instances, object)    # assert that reservation contains ec2 instances
+instances = reservations[0].instances                   # get list of ec2 instances
+instance = instances[0]                                 # get the first ec2 instance
 
-try:
-    key = ec2.get_all_key_pairs(keynames=[key_name])[0]
-except ec2.ResponseError,e :
-    if e.code == 'InvalidKeyPair.NotFound':
-        print 'Creating KeyPair: %s' % key_name
 
-        key = ec2.create_key_pair(key_name)
-        key.save(key_dir)
-    else:
-        raise
-
-reservations = ec2.run_instances(
-    image_id='ami-d0d8b8e0',
-    key_name=key_name,
-    instance_type='t1.micro',
-    security_groups='ssh-access'
-)
-
+print "Instance id: "
+print "Hostname: "
+print "Instance type: " + instance.instance_type
+print "Location: " + instance.placement
+print "Time of launch: "
 
 
 
