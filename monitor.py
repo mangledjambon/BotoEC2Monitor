a46e3e29f9646aeb547722085380545ffbb44f94
sean <seanbreen91@gmail.com>
2015-08-06 11:59:54 +0100

    connect to s3 and list bucket names

--- monitor.py
+++ monitor.py
@@ -5,31 +5,36 @@ __author__ = 'sean'
 
 import boto.ec2
 
-ec2 = boto.connect_ec2()           # connect to region where instance is located
-regions = ec2.get_all_regions()
+ec2 = boto.connect_ec2()                            # connect to EC2, returns connection object
+regions = ec2.get_all_regions()                     # get list of all available regions
 
-for r in regions:
-    connection = boto.connect_ec2(region=r)
+for r in regions:                                   # loop through all regions in list
+    connection = boto.connect_ec2(region=r)         # make a connection to the region
     print r.name
-    reservations = connection.get_all_instances()               # get list of all reservations
+    reservations = connection.get_all_instances()   # get list of all reservations
+
+    # loop through all reservations and for each reservation, return a list of instances
     instances = [index for reservation in reservations for index in reservation.instances]
 
-    if instances.__len__() < 1:
-        print "No instances in this region"
+    if instances.__len__() < 1:                     # check if theres less than one instance in the region
+        print "No instances running in this region"
         print "==================================="
-    else:
-        for instance in instances:
+    else:                                           # else, there must be at least one so
+        for instance in instances:                  # loop through all instances, print details
             print "ID: \t\t\t" + instance.id
             print "Instance Type:\t\t" + instance.instance_type
             print "Launch Time: \t\t" + instance.launch_time
             print "==================================="
 
-s3 = boto.connect_s3()
+print "Connecting to Amazon S3...\n"
+s3 = boto.connect_s3()                              # connect to S3 storage system
 
-buckets = s3.get_all_buckets()
+print "Retreiving bucket information...\n"
+buckets = s3.get_all_buckets()                      # get list of all buckets for this user
 
-for bucket in buckets:
-    print bucket.name
+for bucket in buckets:                              # loop through all buckets
+    print "Bucket name: "
+    print bucket.name                               # print bucket name
 
 
 
